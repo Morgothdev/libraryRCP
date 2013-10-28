740271f107e31c61d1c23fd2a810d820059ffc6b
Morgarooth <mateuszjaje@gmail.com>
2013-10-28 17:06:16 +0100

    pusta deklaracja XMLBooksDataBase

---
+++ src/libraryRCP/data/XMLBooksDataBase.java
@@ -0,0 +1,43 @@
+package libraryRCP.data;
+
+import java.util.List;
+import java.util.Properties;
+
+public class XMLBooksDataBase extends AbstractBooksDataBase{
+
+	public XMLBooksDataBase(Properties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addBook(Book newBook) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeBook(Book bookToDelete) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Book getBookByID(long bookID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Book getBook(String author, String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
